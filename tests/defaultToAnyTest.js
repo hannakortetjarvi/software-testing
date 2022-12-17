952cdf7de24633aaac32ee90838cc2314f91a858
hannakortetjarvi <kortetjarvihanna@gmail.com>
2022-12-17 23:10:39 +0200

    Add defaultToAny tests

---
+++ tests/defaultToAnyTest.js
@@ -0,0 +1,25 @@
+import defaultToAny from '../src/defaultToAny.js';
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('defaultToAny.js', function() {
+    it('should return original integer value', function() {
+        const result = defaultToAny(1, 10, 20);
+        expect(result).to.equal(1);
+    });
+
+    it('should return first default', function() {
+        const result = defaultToAny(undefined, 10, 20);
+        expect(result).to.equal(10);
+    });
+
+    it('should return second default', function() {
+        const result = defaultToAny(undefined, null, 20);
+        expect(result).to.equal(20);
+    });
+
+    it('should return original string value', function() {
+        const result = defaultToAny("hei", "hoi", 1);
+        expect(result).to.equal("hei");
+    });
+});
